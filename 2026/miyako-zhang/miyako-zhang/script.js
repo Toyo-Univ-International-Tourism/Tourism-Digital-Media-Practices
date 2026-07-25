c993e0a085e902914010d0612654b54c1d158034
s3e102500068-sys <s3e102500068@toyo.jp>
2026-07-25 13:57:32 +0900

    Add files via upload

---
+++ 2026/miyako-zhang/miyako-zhang/script.js
@@ -0,0 +1,13 @@
+const btn = document.getElementById("tourBtn");
+
+btn.addEventListener("click", function(){
+
+    alert("バーチャル旅行を始める！");
+
+    window.open(
+        "https://viewtoly-mvp-462744124914.asia-northeast1.run.app/v/x2sxdRrKv-d5",
+        "_blank"
+    );
+
+});
+
